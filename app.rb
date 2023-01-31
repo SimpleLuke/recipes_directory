c4fc8cb1a096f8813ce9fbadda1e22b743dcce65
Luke Lai <lukelai.dev@gmail.com>
2023-01-31 10:18:42 +0000

    Update the app.rb to print out the recipes list from the database

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 # file: app.rb
-
 require_relative 'lib/database_connection'
+require_relative 'lib/recipe_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('recipes_directory')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, name, cooking_time, rating FROM  recipes;'
-result = DatabaseConnection.exec_params(sql, [])
+# sql = 'SELECT id, name, cooking_time, rating FROM  recipes;'
+# result = DatabaseConnection.exec_params(sql, [])
+
+repo = RecipeRepository.new
+
+repo.all.each do |recipe|
+  puts "#{recipe.id} - #{recipe.name} - #{recipe.cooking_time} - #{recipe.rating}"
+end
 
 # Print out each record from the result set .
-result.each do |record|
-  p record
-end
+# result.each do |record|
+#   p record
+# end
